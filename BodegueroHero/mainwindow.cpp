1eb46174b9156c099a636c9bb214e67792a5a327
Erick Hernandez <erick_160890@hotmail.com>
2013-01-30 13:00:23 -0600

    Stylesheet del proyecto agregado.

--- BodegueroHero/mainwindow.cpp
+++ BodegueroHero/mainwindow.cpp
@@ -1,17 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QFile"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    QFile file(":/qss/style.qss");
+    if(file.open(QFile::ReadOnly)) {
+       QString StyleSheet = QLatin1String(file.readAll());
+       qApp->setStyleSheet(StyleSheet);
+    }
 
     ui->setupUi(this);    
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;        
+    delete ui;            
 }
 
 
